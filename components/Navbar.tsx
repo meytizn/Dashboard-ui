49da677f8236aadf5541a4900b9d8f49c68e67e6
meytizn <zamanianmehdi23desktop@gmail.com>
2026-06-01 22:07:59 +0330

    Navbar format edited

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,81 +2,81 @@ import React from 'react'
 import Image from "next/image";
 import Link from "next/link"
 import logo from '../img/logo.png'
-import { Avatar,AvatarFallback,AvatarImage} from "@/components/ui/avatar"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Button } from "@/components/ui/button"
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 
 
 
 const Navbar = () => {
-  return (
-    <div className='w-full min-h-13 py-2 px-5 flex items-center justify-between dark:bg-red-800 bg-black text-white'>
-      <Link  href="/">
-      <Image className='rounded-full' src={logo} alt="dashboard" width={50} />
-      </Link>
-
-
- 
-      
+    return (
+        <div className='w-full min-h-13 py-2 px-5 flex items-center justify-between dark:bg-red-800 bg-black text-white'>
+            <Link href="/">
+                <Image className='rounded-full' src={logo} alt="dashboard" width={50} />
+            </Link>
 
 
 
 
 
-<DropdownMenu>
-  <DropdownMenuTrigger className='focus:outline-none'>
-
-
-
-     
-          <Avatar>
-      <AvatarImage
-        src="https://github.com/shadcn.png"
-        alt="@shadcn"
-        className="grayscale"
-      />
-      <AvatarFallback>AC</AvatarFallback>
-    </Avatar>
 
 
 
 
-
-  </DropdownMenuTrigger>
-  <DropdownMenuContent>
-    <DropdownMenuGroup>
+            <DropdownMenu>
+                <DropdownMenuTrigger className='focus:outline-none'>
 
 
-      <DropdownMenuLabel>My Account</DropdownMenuLabel>
-      <DropdownMenuItem><Link href='/profile'>Profile</Link></DropdownMenuItem>
-      <DropdownMenuItem><Link href='/auth'>Logout</Link></DropdownMenuItem>
+
+
+                    <Avatar>
+                        <AvatarImage
+                            src="https://github.com/shadcn.png"
+                            alt="@shadcn"
+                            className="grayscale"
+                        />
+                        <AvatarFallback>AC</AvatarFallback>
+                    </Avatar>
 
 
 
 
 
-    </DropdownMenuGroup>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                    <DropdownMenuGroup>
 
-    {/* <DropdownMenuSeparator />
+
+                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                        <DropdownMenuItem><Link href='/profile'>Profile</Link></DropdownMenuItem>
+                        <DropdownMenuItem><Link href='/auth'>Logout</Link></DropdownMenuItem>
+
+
+
+
+
+                    </DropdownMenuGroup>
+
+                    {/* <DropdownMenuSeparator />
     <DropdownMenuGroup>
       <DropdownMenuItem>Team</DropdownMenuItem>
       <DropdownMenuItem>Subscription</DropdownMenuItem>
     </DropdownMenuGroup> */}
 
-  </DropdownMenuContent>
-</DropdownMenu>
+                </DropdownMenuContent>
+            </DropdownMenu>
 
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Navbar
